341c1f1e896eeca724a90557647a460c1c600a91
Adriz1611 <adrijbhadra@gmail.com>
2024-07-14 14:58:18 +0530

    70% home page done

--- src/app/page.js
+++ src/app/page.js
@@ -3,33 +3,68 @@ import Image from "next/image";
 export default function Home() {
   return (
     <main className="flex flex-col min-h-screen">
-      <nav className="flex items-center justify-between w-full p-4 bg-white shadow-md">
-        <Image
-          src="/logo.png"
-          alt="ChangeSpark Foundation logo"
-          width={150}
-          height={100}
-        />
-        <ul className="flex space-x-12">
-          {["Home", "Skills", "Projects", "Contact", "Donate"].map((item) => (
-            <li key={item} className="list-none">
-              <a
-                href={`#${item.toLowerCase()}`}
-                className="text-base font-medium text-gray-900 capitalize hover:after:content-[''] hover:after:block hover:after:w-full hover:after:h-0.5 hover:after:bg-black hover:after:m-auto"
-              >
-                {item}
-              </a>
-            </li>
-          ))}
-        </ul>
-      </nav>
+      <nav className="flex items-center justify-between bg-beige h-20 pr-20 font-sans">
+    <div className="font-bold text-xl flex flex-row items-center">
+    <Image src="/logo.png" alt="ChangeSpark Foundation logo" width={100} height={100} className="h-min w-40 pl-5 bg-transparent"/>
+    <h1 className="text-purp">ChangeSpark<span className="text-gren"> Foundation</span></h1></div>
+    <ul className="flex space-x-20">
+      <li className="list-none">
+        <a
+          href="/"
+          className="text-base font-medium text-gray-900 capitalize hover:after:content-[''] hover:after:block hover:after:w-full hover:after:h-0.5 hover:after:bg-black hover:after:m-auto"
+        >
+          HOME
+        </a>
+      </li>
+      <li className="list-none">
+        <a
+          href="#about"
+          className="text-base font-medium text-gray-900 capitalize hover:after:content-[''] hover:after:block hover:after:w-full hover:after:h-0.5 hover:after:bg-black hover:after:m-auto"
+        >
+          ABOUT US
+        </a>
+      </li>
+      <li className="list-none">
+        <a
+          href="#team"
+          className="text-base font-medium text-gray-900 capitalize hover:after:content-[''] hover:after:block hover:after:w-full hover:after:h-0.5 hover:after:bg-black hover:after:m-auto"
+        >
+          TEAM
+        </a>
+      </li>
+      <li className="list-none">
+        <a
+          href="#projects"
+          className="text-base font-medium text-gray-900 capitalize hover:after:content-[''] hover:after:block hover:after:w-full hover:after:h-0.5 hover:after:bg-black hover:after:m-auto"
+        >
+          PROJECTS
+        </a>
+      </li>
+      <li className="list-none">
+        <a
+          href="#join-us"
+          className="text-base font-medium text-gray-900 capitalize hover:after:content-[''] hover:after:block hover:after:w-full hover:after:h-0.5 hover:after:bg-black hover:after:m-auto"
+        >
+          JOIN US
+        </a>
+      </li>
+      <li className="list-none">
+        <a
+          href="#donate"
+          className="text-base font-medium text-gray-900 capitalize hover:after:content-[''] hover:after:block hover:after:w-full hover:after:h-0.5 hover:after:bg-black hover:after:m-auto"
+        >
+          DONATE
+        </a>
+      </li>
+    </ul>
+  </nav>
 
       <div className="flex flex-col md:flex-row justify-between items-center p-8 bg-gray-100 flex-grow">
-        <div className="md:w-1/2 mb-8 md:mb-0">
-          <h1 className="text-4xl font-bold text-red-800 mb-4">
+        <div className="md:w-1/2 mb-8 md:mb-0 ">
+          <h1 className="text-4xl font-bold text-purple-900 dark:font-semibold mb-4">
             Changespark Foundation
           </h1>
-          <p className="text-xl text-yellow-500 font-semibold">
+          <p className="text-xl text-green-700 dark:font-semibold">
             EMPOWERING POWER IGNITING CHANGE
           </p>
         </div>
--- tailwind.config.js
+++ tailwind.config.js
@@ -9,8 +9,18 @@ module.exports = {
     extend: {
       backgroundImage: {
         "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+        "gradient-conic": "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+      },
+      colors: {
+        green1: {
+          light: "#5EA384",
+          dark: "#0E6E44", // added missing #
+        },
+        purple1: {
+          light: "#8E68A5",
+          dark: "#623B7A", // added missing #
+        },
+        blue: "#1A3641", // added missing #
       },
     },
   },
