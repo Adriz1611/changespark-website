382380fb17646489dde02ae36f555c53329e58c5
AriaFantom <priyanshudebnath348@gmail.com>
2024-07-20 21:02:10 +0530

    Added Redirect to cards

--- src/app/projects/page.js
+++ src/app/projects/page.js
@@ -1,10 +1,12 @@
 import Image from "next/image";
 import { MoveUpRight } from "lucide-react";
- 
+import Link from "next/link";
+
 const Projects = () => {
   const images = [
     {
       src: "/bhadreswar.jpg",
+      url: "/projects/bhadreswar",
       alt: "Women at Tailoring",
       caption: "Women at Tailoringg",
       description:
@@ -13,6 +15,7 @@ const Projects = () => {
     {
       src: "/goghat.jpg",
       alt: "Children at School",
+      url: "/projects/goghat",
       caption: "Children at School",
       description:
         "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
@@ -26,26 +29,28 @@ const Projects = () => {
       </h1>
       <div className="flex flex-col md:flex-row gap-10 md:gap-40 ">
         {images.map((img, index) => (
-          <div
-            key={index}
-            className=" relative flex flex-col items-start text-start w-[450px] bg-background-200 px-4 py-4 rounded-md shadow-md cursor-pointer"
-          >
-            <MoveUpRight size={32} className="text-green-300 absolute top-5 right-5" />
-            <Image
-              src={img.src}
-              alt={img.alt}
-              height={300}
-              width={450}
-              className="rounded-md shadow-md"
-            />
+          <Link href={img.url} key={index}>
+            <div className="group relative flex flex-col items-start text-start w-[450px] bg-background-200 px-4 py-4 rounded-md shadow-md cursor-pointer">
+              <MoveUpRight
+                size={32}
+                className="text-green-300 absolute top-5 right-5 opacity-0 group-hover:opacity-100 transform translate-y-4 group-hover:translate-y-0 transition-all duration-500"
+              />
+              <Image
+                src={img.src}
+                alt={img.alt}
+                height={300}
+                width={450}
+                className="rounded-md shadow-md"
+              />
 
-            <h2 className="mt-4 uppercase font-bold text-xl font-paragraph text-green-700 text-center">
-              {img.caption}
-            </h2>
-            <p className="text-wrap font-paragraph text-secondary-700">
-              {img.description}
-            </p>
-          </div>
+              <h2 className="mt-4 uppercase font-bold text-xl font-paragraph text-green-700 text-center">
+                {img.caption}
+              </h2>
+              <p className="text-wrap font-paragraph text-secondary-700">
+                {img.description}
+              </p>
+            </div>
+          </Link>
         ))}
       </div>
     </div>
