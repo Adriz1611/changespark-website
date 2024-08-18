eaf8ed99fcff26979ccdbe9d3231c9d215bb30ef
Monodeep-Das <m.monodas@gmail.com>
2024-08-18 22:26:50 +0530

    New Design

--- src/app/blog/[id]/page.js
+++ src/app/blog/[id]/page.js
@@ -1,8 +1,3 @@
-/**
- * v0 by Vercel.
- * @see https://v0.dev/t/FGH4q2r2OUi
- * Documentation: https://v0.dev/docs#integrating-generated-code-into-your-nextjs-app
- */
 import Link from "next/link"
 import { Button } from "@/components/ui/button"
 
