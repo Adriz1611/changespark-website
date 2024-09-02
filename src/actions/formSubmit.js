0d45648f175065740c9e91073d7b585892a4f6ed
AriaFantom <priyanshudebnath348@gmail.com>
2024-09-02 19:58:19 +0530

    Done Form Submission

--- src/actions/formSubmit.js
+++ src/actions/formSubmit.js
@@ -1,13 +1,33 @@
 "use server"
 
 import {createClient} from "@/utils/supabase/server";
+import {redirect} from "next/navigation";
 
 export default async function submitForm(data) {
     const supabase = createClient()
 
-    const { data: formData, error} = await supabase.from("applications").insert([
-        { "fullname": ""}
-        ]
-    ).select()
+
+    const {error} = await supabase.from("applications").insert(
+        {
+            "fullname": data.fullName,
+            "email": data.email,
+            "phoneNumber": data.phoneNumber,
+            "address": data.address,
+            "availability": data.availability,
+            "interests": data.interests,
+            "preferredDays": data.preferredDays,
+            "skills": data.skills,
+            "experience": data.experience,
+            "startDate": data.startDate,
+            "endDate": data.endDate,
+            "qualifications": data.qualifications,
+            "reason": data.reason,
+            "contribute": data.contribute,
+        },
+    )
+
+    if (error) throw error
+
+    redirect("/")
 
 }
