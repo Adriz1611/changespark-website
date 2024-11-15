aa7d837080be4511a10b1160e25c6943c72be343
Adriz1611 <adrijbhadra@gmail.com>
2024-11-15 11:30:22 +0530

    anek ta hoyegelo

--- src/app/projects/page.js
+++ src/app/projects/page.js
@@ -1,6 +1,7 @@
 "use client";
 
 import Image from "next/image";
+import Link from "next/link";
 import { motion } from "framer-motion";
 
 const Projects = () => {
@@ -11,6 +12,7 @@ const Projects = () => {
       caption: "Natun Pata",
       description:
         "ChangeSpark Foundation Education Program nurtures young minds by delivering quality early childhood education in underserved rural areas, fostering cognitive, social, and emotional growth, and empowering communities to value lifelong learning.",
+      href: "/projects/natunpata"
     },
     {
       src: "/bhadreswar.avif",
@@ -18,6 +20,7 @@ const Projects = () => {
       caption: "Pranati",
       description:
         "ChangeSpark Foundation Entrepreneurship Development Program empowers individuals, especially women, with vocational and business skills, fostering economic independence, local job creation, and sustainable community growth through hands-on training and market access.",
+      href: "/projects/pranati"
     },
   ];
 
@@ -56,30 +59,31 @@ const Projects = () => {
       </motion.h1>
       <div className="flex flex-col md:flex-row gap-16 items-center justify-evenly w-full px-14 mt-5">
         {images.map((img, index) => (
-          <motion.div
-            key={index}
-            variants={itemVariants}
-            className="group relative flex flex-col items-start text-start md:mx-0 mx-2 bg-background-200 p-4 rounded-md shadow-md w-full md:w-1/2 max-w-lg"
-            whileHover={{ scale: 1.05 }}
-            transition={{ duration: 0.3 }}
-          >
-            <div className="w-full h-auto">
-              <Image
-                src={img.src}
-                alt={img.alt}
-                layout="responsive"
-                width={800}
-                height={600}
-                className="rounded-md shadow-md"
-              />
-            </div>
-            <h2 className="mt-4 uppercase font-bold text-xl font-paragraph text-green-700 text-center">
-              {img.caption}
-            </h2>
-            <p className="text-wrap font-paragraph text-secondary-700">
-              {img.description}
-            </p>
-          </motion.div>
+          <Link href={img.href} key={index} className="w-full md:w-1/2 max-w-lg">
+            <motion.div
+              variants={itemVariants}
+              className="group relative flex flex-col items-start text-start md:mx-0 mx-2 bg-background-200 p-4 rounded-md shadow-md w-full h-full cursor-pointer"
+              whileHover={{ scale: 1.05 }}
+              transition={{ duration: 0.3 }}
+            >
+              <div className="w-full h-auto">
+                <Image
+                  src={img.src}
+                  alt={img.alt}
+                  layout="responsive"
+                  width={800}
+                  height={600}
+                  className="rounded-md shadow-md"
+                />
+              </div>
+              <h2 className="mt-4 uppercase font-bold text-xl font-paragraph text-green-700 text-center">
+                {img.caption}
+              </h2>
+              <p className="text-wrap font-paragraph text-secondary-700">
+                {img.description}
+              </p>
+            </motion.div>
+          </Link>
         ))}
       </div>
     </motion.div>
