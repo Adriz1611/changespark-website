8613f0bde7bc156e87b8b9000d5d6ff14d351530
Adriz1611 <adrijbhadra@gmail.com>
2025-09-19 21:04:48 +0530

    Refactor formSubmit.js for improved code formatting and consistency

--- src/actions/formSubmit.js
+++ src/actions/formSubmit.js
@@ -1,33 +1,30 @@
-"use server"
+"use server";
 
-import {createClient} from "@/utils/supabase/server";
-import {redirect} from "next/navigation";
+import { createClient } from "@/utils/supabase/server";
+import { redirect } from "next/navigation";
 
 export default async function submitForm(data) {
-    const supabase = createClient()
+  const supabase = createClient();
 
+  const { error } = await supabase.from("applications").insert({
+    fullname: data.fullName,
+    email: data.email,
+    phoneNumber: data.phoneNumber,
+    address: data.address,
+    availability: data.availability,
+    interests: data.interests,
+    preferredDays: data.preferredDays,
+    skills: data.skills,
+    experience: data.experience,
+    startDate: data.startDate,
+    endDate: data.endDate,
+    qualifications: data.qualifications,
+    reason: data.reason,
+    contribute: data.contribute,
+    isActive: true, // Default to true for new applications, can be managed by admin panel
+  });
 
-    const {error} = await supabase.from("applications").insert(
-        {
-            "fullname": data.fullName,
-            "email": data.email,
-            "phoneNumber": data.phoneNumber,
-            "address": data.address,
-            "availability": data.availability,
-            "interests": data.interests,
-            "preferredDays": data.preferredDays,
-            "skills": data.skills,
-            "experience": data.experience,
-            "startDate": data.startDate,
-            "endDate": data.endDate,
-            "qualifications": data.qualifications,
-            "reason": data.reason,
-            "contribute": data.contribute,
-        },
-    )
+  if (error) throw error;
 
-    if (error) throw error
-
-    redirect("/")
-
+  redirect("/");
 }
